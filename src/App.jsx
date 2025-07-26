edbfc460b6b26f0e00dfdfbb73c9782ceb6ae189
Dan Rozén <dan.rozen@bjornlunden.com>
2025-07-26 20:46:26 +0200

    Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+        <h1>todo-app</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Your app is live! Start building something amazing!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
